478195d2804278bb0d273582d96121df1df92c14
Asan Sirwan <asanlovesyou@gmail.com>
2024-06-15 13:46:35 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+	import { RouterView } from "vue-router";
+</script>
+
+<template>
+	<!-- <header class="fixed start-0 top-0 w-full">
+		<nav class="flex items-center gap-4 py-4 px-8">
+			<div class="w-12 h-12">
+				<img src="/public/images/logo.png" />
+			</div>
+			<h1 class="text-xl text-white">Asan Sirwan</h1>
+		</nav>
+	</header> -->
+	<main>
+		<RouterView />
+	</main>
+</template>
---
+++ src/views/IndexView.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+	<header class="w-full h-screen flex flex-col items-center justify-center">
+		<h1 class="text-6xl text-white font-bold cursor-default tracking-wide select-none welcome-text-after">
+			Welcome!
+		</h1>
+	</header>
+</template>
